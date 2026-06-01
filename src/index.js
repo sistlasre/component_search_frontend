b007fea6b50ce72d62c88587617593392afe71a5
Sreeharsha Sistla <sreeharsha.sistla@gmail.com>
2026-06-01 15:56:10 -0700

    Adding GA4 template

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+// Initialize GA4 with your Measurement ID
+ReactGA.initialize('G-XXXXXXXXXX');
+// Send the initial pageview (which grabs the UTMs from the URL)
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
