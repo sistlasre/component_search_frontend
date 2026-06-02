f98bd4563c3ba58ceb4f8241632a3a530845bc74
Sreeharsha Sistla <sreeharsha.sistla@gmail.com>
2026-06-02 14:36:46 -0700

    Adding measurement id

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import ReactGA from 'react-ga4';
 
 // Initialize GA4 with your Measurement ID
-ReactGA.initialize('G-XXXXXXXXXX');
+ReactGA.initialize('G-BE3QMHZ0JD');
 // Send the initial pageview (which grabs the UTMs from the URL)
 ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 
