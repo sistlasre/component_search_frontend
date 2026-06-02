363df73f5ce8a9a48c3d4cb56350b05aa8f19452
Sreeharsha Sistla <sreeharsha.sistla@gmail.com>
2026-06-02 15:28:09 -0700

    Fixing gtag options for ga4

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import './index.css';
 import App from './App';
 import ReactGA from 'react-ga4';
 
-// Initialize GA4 with your Measurement ID
-ReactGA.initialize('G-BE3QMHZ0JD', {
-  gtagOptions: {
-    ad_storage: 'granted',
-    analytics_storage: 'granted',
-    ad_user_data: 'granted',
-    ad_personalization: 'granted'
-  }
+
+// 1. Define the global gtag function manually to force browser-level consent
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+
+gtag('consent', 'default', {
+  'ad_storage': 'granted',
+  'analytics_storage': 'granted',
+  'ad_user_data': 'granted',
+  'ad_personalization': 'granted'
 });
+// Initialize GA4 with your Measurement ID
+ReactGA.initialize('G-BE3QMHZ0JD');
 // Send the initial pageview (which grabs the UTMs from the URL)
 ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 
